99ead631f0af3b935baf9fae6633e0509632a605
smaule1 <143682799+smaule1@users.noreply.github.com>
2025-12-01 18:54:18 -0600

    confirmacion del driver

--- driver.c
+++ driver.c
@@ -10,86 +10,94 @@
     ReadFile(port_handle, receive_buffer, sizeof(receive_buffer),
     &bytes_received, NULL);
     if(bytes_received > 0){
-        if(strcmp(receive_buffer, "ping") == 0){            
+        if(strcmp(receive_buffer, "ping") == 0){
             char* str = "pong";
             WriteFile(port_handle, str, strlen(str), NULL, NULL);
             return TRUE;
         }
         printf("%s\n", receive_buffer);
-    }        
+    }
     return FALSE;
 }*/
 
-int read_btn(HANDLE port_handle){
-    char receive_buffer[64] = {0};
+int read_btn(HANDLE port_handle) {
+    char receive_buffer[64] = { 0 };
     int value;
     DWORD bytes_received;
     ReadFile(port_handle, receive_buffer, sizeof(receive_buffer),
-    &bytes_received, NULL);
-    if(bytes_received > 0){
+        &bytes_received, NULL);
+    if (bytes_received > 0) {
         printf("%s\n", receive_buffer);
-        if(strcmp(receive_buffer, "1") == 0){            
-            ShellExecute(NULL, "open", "taskmgr.exe", NULL, NULL, SW_SHOWNORMAL);
+        if(receive_buffer[0] >= '1' && receive_buffer[0] <= '4') {
+            WriteFile(port_handle, "r", 1, NULL, NULL);
+        }
+
+        if (strcmp(receive_buffer, "1") == 0) {
+            ShellExecute(NULL, L"open", L"taskmgr.exe", NULL, NULL, SW_SHOWNORMAL);
             return TRUE;
-        } else if (strcmp(receive_buffer, "2") == 0){
+        }
+        else if (strcmp(receive_buffer, "2") == 0) {
             SetSuspendState(FALSE, TRUE, FALSE);
-        } else if (strcmp(receive_buffer, "3") == 0){
+        }
+        else if (strcmp(receive_buffer, "3") == 0) {
             system("shutdown /r /t 0");
-        } else if (strcmp(receive_buffer, "4") == 0){
+        }
+        else if (strcmp(receive_buffer, "4") == 0) {
             system("shutdown /s /t 0");
         }
-       return TRUE;
+        return TRUE;
     }
     return FALSE;
 }
 
 
 
-int main(){
+int main() {
     //Crear handle del puerto
     HANDLE port_handle;
-    port_handle = CreateFileA("COM7", GENERIC_READ | GENERIC_WRITE,
-    0, NULL, OPEN_EXISTING,0, NULL);
+    port_handle = CreateFileA("COM6", GENERIC_READ | GENERIC_WRITE,
+        0, NULL, OPEN_EXISTING, 0, NULL);
 
-    if(port_handle == INVALID_HANDLE_VALUE){
-        printf("no sirvió .-.\n");
+    if (port_handle == INVALID_HANDLE_VALUE) {
+        printf("Error\n");
         char err_msg[256];
         DWORD err_num = GetLastError();
         FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL, err_num, 0, err_msg, sizeof(err_msg), NULL);
+            NULL, err_num, 0, err_msg, sizeof(err_msg), NULL);
         printf("ERROR: %s\n", err_msg);
-    }else{
-        printf("sí sirvio :)\n");        
+    }
+    else {
+        printf("Connected to COM port\n");
     }
 
     // Configurar puerto COM
-    DCB port_config = {0};
+    DCB port_config = { 0 };
     port_config.DCBlength = sizeof(port_config);
-    
-    int status = GetCommState(port_handle, &port_config);
+
+    int status = GetCommState(port_handle, &port_config);    
 
     //config
     port_config.BaudRate = 9600;
     port_config.ByteSize = 8;
     port_config.Parity = NOPARITY;
     port_config.StopBits = ONESTOPBIT;
-    SetCommState(port_handle, &port_config);
+    SetCommState(port_handle, &port_config);    
 
-    COMMTIMEOUTS timeouts = {0};
+    COMMTIMEOUTS timeouts = { 0 };
     timeouts.ReadIntervalTimeout = 20;
     timeouts.ReadTotalTimeoutMultiplier = 1;
     timeouts.ReadTotalTimeoutConstant = 50;
     timeouts.WriteTotalTimeoutConstant = 50;
     timeouts.WriteTotalTimeoutMultiplier = 1;
-    SetCommTimeouts(port_handle, &timeouts);
-    
+    SetCommTimeouts(port_handle, &timeouts);    
+
     // Receive Loop
-    SetCommMask(port_handle, EV_RXCHAR);
-    DWORD event_mask;
-    int connected = FALSE;
-    while(TRUE){
-        WaitCommEvent(port_handle, &event_mask, NULL);
-        if(event_mask & EV_RXCHAR){
+    SetCommMask(port_handle, EV_RXCHAR);    
+    DWORD event_mask = 0;
+    while (TRUE) {
+        status = WaitCommEvent(port_handle, &event_mask, NULL);        
+        
+        if (event_mask & EV_RXCHAR) {            
             read_btn(port_handle);
         }
     }
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3f, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int btnTask = 3;
 int btnSuspend = 4;
@@ -9,54 +9,110 @@ int btnRestart = 5;
 int btnShutdown = 6;
 
 void setup() {
-  Serial.begin(9600); // Start serial communication at 9600 baud
-  while (!Serial);    // Wait for serial port to be ready 
+  Serial.begin(9600);  // Start serial communication at 9600 baud
+  while (!Serial); 
   lcd.init();
   lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Feliz Jueves :)");
-  lcd.setCursor(0,1);
-  lcd.print("");
 }
 
-bool connected = false;
+int timeout = 0;
+int pressed = 0;
 
-void loop() {  
-  lcd.setCursor(0,1);
-  if(digitalRead(btnTask) == HIGH){
+void loop() {
+  lcd.setCursor(0, 1);
+  if (timeout == 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if (pressed != 0) {
+      lcd.print("Error");
+      pressed = 0;
+      timeout = 2000;
+    } else {
+      lcd.print("Press any button");
+    }
+  }
+
+  if (pressed == 0) {
+    handleButtonPressed();
+  } else {
+    if (Serial.available() > 0) {
+      String str = Serial.readString();
+      if (str == "r") {
+        printText(pressed);
+        pressed = 0;
+      }
+    }
+  }
+
+  lcd.display();
+  if(timeout >= 0){
+    timeout -= 100;
+  }
+  delay(100);
+}
+
+void handleButtonPressed() {
+
+  if (digitalRead(btnTask) == HIGH) {
     Serial.print("1");
     lcd.clear();
 
-    lcd.setCursor(0,0);
-    lcd.print("Open TaskManager");
-    lcd.setCursor(0,1);
-    lcd.print("Button 1 Pressed");
-  } else if (digitalRead(btnSuspend) == HIGH){
+    lcd.setCursor(0, 0);
+    lcd.print("Button 1 Pressed...");
+    timeout = 2000;
+    pressed = 1;
+  } else if (digitalRead(btnSuspend) == HIGH) {
+    Serial.print("2");
     lcd.clear();
 
-    Serial.print("2");
-    lcd.setCursor(0,0);
-    lcd.print("PC suspended");
-    lcd.setCursor(0,1);
-    lcd.print("Button 2 Pressed");
-  } else if (digitalRead(btnRestart) == HIGH){
+    lcd.setCursor(0, 0);
+    lcd.print("Button 2 Pressed...");
+    timeout = 2000;
+    pressed = 2;    
+  } else if (digitalRead(btnRestart) == HIGH) {
     Serial.print("3");
-    
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Restarting PC");
-    lcd.setCursor(0,1);
-    lcd.print("Button 3 Pressed");
-  } else if (digitalRead(btnShutdown) == HIGH){
+
+    lcd.setCursor(0, 0);
+    lcd.print("Button 3 Pressed...");
+    timeout = 2000;
+    pressed = 3;
+  } else if (digitalRead(btnShutdown) == HIGH) {
     Serial.print("4");
-    
     lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Shutting down PC");
-    lcd.setCursor(0,1);
-    lcd.print("Button 4 Pressed");
+
+    lcd.setCursor(0, 0);
+    lcd.print("Button 4 Pressed...");
+    timeout = 2000;
+    pressed = 4;
   }
-  lcd.display();
-  delay(100);
+}
+
+
+void printText(int n) {
+  if (n == 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Open TaskManager");
+    lcd.setCursor(0, 1);
+    timeout = 2000;
+  } else if (n == 2){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("PC Suspended");
+    lcd.setCursor(0, 1);
+    timeout = 2000;
+  } else if (n == 3){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Restarting PC");
+    lcd.setCursor(0, 1);
+    timeout = 2000;
+  } else if (n == 4){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Shutting down PC");
+    lcd.setCursor(0, 1);
+    timeout = 2000;
+  } 
 }
